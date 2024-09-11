4179a5da5ea5aa1ea0a496bdb0728baa87a93bf1
DANIEL PAIM FRANTZ <daniel.paim@edu.pucrs.br>
2024-09-11 18:19:21 -0300

    Double linked listas

---
+++ ALEST/List Nodo Double Linked/DoublyLinkedList.java
@@ -0,0 +1,140 @@
+public class DoublyLinkedList implements ListTAD {
+
+    private Node header;
+    private Node trailer;
+    private int count;
+
+    private class Node {
+        public int item;
+        public Node previous;
+        public Node next;
+
+        public Node(int element) {
+            this.item = element;
+            this.previous = null;
+            this.next = null;
+        }
+
+        public Node(int element, Node previous, Node next) {
+            this.item = element;
+            this.previous = previous;
+            this.next = next;
+        }
+    }
+    public DoublyLinkedList()
+    {
+        //Implementar
+    }
+    
+    @Override
+    public void add(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        Node newNode=new Node(element);
+
+    }
+
+    @Override
+    public void add(int index, int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void addFirst(int e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addFirst'");
+    }
+
+    @Override
+    public int get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public int getFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFirst'");
+    }
+
+    @Override
+    public int getLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLast'");
+    }
+
+    @Override
+    public int set(int index, int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public boolean remove(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int removeByIndex(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeByIndex'");
+    }
+
+    @Override
+    public int removeFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+    }
+
+    @Override
+    public int removeLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public boolean contains(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public int indexOf(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'indexOf'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder ("[");
+        Node atual=header.next;
+        while(atual!=trailer)
+        {
+            sb.append(atual.item).append(" ");
+            atual=atual.next;
+        }
+        sb.append("]")    
+        return sb.toString();
+    }
+
+}
