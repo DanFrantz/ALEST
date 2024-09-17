b0879fcf79a7aca23738b8041409887e5154f96e
Daniel Frantz <danielpaimf@gmail.com>
2024-09-17 13:29:39 -0300

    Adicionei o contador de ocorrências

--- ListaOrdenada/ListOrdArray.java
+++ ListaOrdenada/ListOrdArray.java
@@ -234,4 +234,78 @@
             }
         }
     }
+    
+
+    /*Acrescente na classe ListArray o seguinte método: int countOccurrences(int
+    element), que conta o número de ocorrências do elemento passado como
+    parâmetro na lista, retornando este valor. Analise a complexidade do método e
+    acrescente a notação O como comentário no código fonte junto da assinatura do
+    método.*/
+    public int countOcurrences(int element){
+        int contador=0;
+        for(int i=0;i<count;i++){
+            if(element==data[i]){
+                contador++;
+            }
+        }return contador;
+    }
+
+    /*
+    4. Acrescente na classe ListArray um método que faça uma inserção ordenada dos
+    elementos na lista (do menor para o maior). A assinatura deste método deve
+    ser: public void addIncreasingOrder(int element). Teste a sua implementação
+    para este método no método main instanciando uma nova lista e acrescentando
+    os elementos usando apenas este método. Analise a complexidade do método e
+    acrescente a notação O como comentário no código fonte junto da assinatura do
+    método. Observação: a lista só ficará garantidamente ordenada se os elementos
+    forem inseridos através da chamada deste método. */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*5. Implemente um método na classe ListArray que percorre a lista e retira os
+    elementos repetidos, deixando apenas uma ocorrência de cada elemento. A
+    assinatura deste método deve ser: public void unique(). Teste a sua
+    implementação para este método na classe App. Analise a complexidade do
+    método e acrescente a notação O como comentário no código fonte junto da
+    assinatura do método. */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*6. Implemente um método que recebe por parâmetro dois ListArray e retorna um
+    terceiro ListArray que contém os elementos resultantes da intersecção das duas
+    listas recebidas por parâmetro. Ou seja, a lista retornada deve conter uma cópia
+    dos elementos que aparecem simultaneamente nas duas listas recebidas por
+    parâmetro. Analise a complexidade do método e acrescente a notação O como
+    comentário no código fonte junto da assinatura do método. */
+    
+    
+    
+    
+    
+    
+    
+    /*7. Instancie duas listas de inteiros (ListArray), e acrescente alguns elementos em
+    cada uma, mantendo-as ordenadas (use o método implementado no exercício
+    4). Depois escreva um método que recebe estas duas listas por parâmetro e
+    retorna uma terceira lista com todos os elementos das duas listas recebidas em
+    ordem crescente. Por exemplo, passando por parâmetro L1 = {3, 5, 8, 13, 19} e
+    L2 = {1, 4, 6, 9, 15}, o resultado deve ser L3 = {1, 3, 4, 5, 6, 8, 9, 13, 15, 19} */
 }
